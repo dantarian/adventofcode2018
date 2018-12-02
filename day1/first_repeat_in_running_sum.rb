3b687ebc2cf91f67551afe109ce5569db547dd38
Warren Jones <warren@pencethren.org>
2018-12-02 15:07:52 +0000

    Refactor solution to Day 1, Puzzle 2 to use shared ArgumentValidator and generally be cleaner.

--- day1/first_repeat_in_running_sum.rb
+++ day1/first_repeat_in_running_sum.rb
@@ -1,65 +1,41 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'immutable'
-require 'sysexits'
+require '../argument_validator'
+require 'set'
 
-# Provides methods for validating input.
-class ArgumentValidator
-  def self.check_usage(args)
-    return if args.length == 1
+USAGE = <<~USAGE
+  Usage: first_repeat_in_running_sum.rb <filename>
 
-    puts 'Usage: first_repeat_in_running_sum.rb <filename>'
-    puts
-    puts 'Given a file containing a series of integers, 1 per line, returns'
-    puts 'the first repeat value obtained by a running sum over the integers,'
-    puts 'looping as necessary.'
-    Sysexits.exit(:usage)
-  end
+  Given a file containing a number of integers, 1 per line, returns the first
+  repeat value obtained by a running sum over the integers, repeating the set
+  of integers as necessary.
+USAGE
 
-  def self.check_file_exists(filename)
-    return if File.exist?(filename)
-
-    puts "File not found: #{filename}"
-    Sysexits.exit(:input_missing)
-  end
-end
+argument_validator = ArgumentValidator.new(USAGE, 1)
 
 # Provides methods for reading a file and finding the first duplicate value
 # in a running sum of its contents, repeating as necessary.
 class DuplicateFrequencyFinder
   def initialize(filename)
-    @filename = filename
-    @running_total = 0
-    @history = Immutable::Set.new
-    @deltas = []
+    @deltas = File.foreach(filename).map(&:chomp).map(&:to_i)
   end
 
   def call
-    read_deltas
+    running_total = 0
+    history = Set[]
+
     loop do
       @deltas.each do |delta|
-        apply_delta(delta)
-        return @running_total unless @history
+        running_total += delta
+        return running_total unless history.add?(running_total)
       end
     end
   end
-
-  private
-
-  def read_deltas
-    File.foreach(@filename) do |line|
-      @deltas << line.chomp.to_i
-    end
-  end
-
-  def apply_delta(delta)
-    @running_total += delta
-    @history = @history.add?(@running_total)
-  end
 end
 
-ArgumentValidator.check_usage(ARGV)
+argument_validator.check_number_of_arguments(ARGV)
 filename = ARGV.first
-ArgumentValidator.check_file_exists(filename)
+argument_validator.check_file_exists(filename)
+
 puts DuplicateFrequencyFinder.new(filename).call
